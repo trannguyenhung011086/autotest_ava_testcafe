be35c7c6fbf25ba6738ae9896a0562d2b3ec4554
Hung <hungtn@leflair.vn>
2019-03-22 16:45:55 +0700

    test

---
+++ tests/e2e_ui/mobile/product.redirect.spec.ts
@@ -0,0 +1,47 @@
+import { t, RequestMock } from "testcafe";
+import { config } from "../../../common/config";
+import * as Utils from "../../../common/utils";
+import { Pages } from "../page_objects";
+
+const requestSale = new Utils.SaleUtils();
+const page = new Pages();
+
+const mockRedirect = RequestMock()
+    .onRequestTo(/\/api\/v2\/product\/(?!view-product)/)
+    .respond({ code: 403 }, 403, { "access-control-allow-origin": "*" });
+
+const mockNonRedirect = RequestMock()
+    .onRequestTo(/\/api\/v2\/product\/(?!view-product)/)
+    .respond(null, 500, { "access-control-allow-origin": "*" });
+
+fixture("Check product detail page redirect").meta({
+    type: "regression"
+});
+
+test.requestHooks(mockRedirect)(
+    "Redirect to homepage when product API returns 403 error code",
+    async () => {
+        const sales = await requestSale.getSales(config.api.featuredSales);
+
+        await t
+            .navigateTo(config.baseUrl + "/vn/sales/" + sales[0].slug)
+            .click(".product-card");
+
+        const location = await t.eval(() => document.location.href);
+        await t.expect(location).eql(config.baseUrl + "/vn");
+    }
+);
+
+test.requestHooks(mockNonRedirect)(
+    "Not redirect to homepage when product API returns 500 error code",
+    async () => {
+        const sales = await requestSale.getSales(config.api.featuredSales);
+
+        await t
+            .navigateTo(config.baseUrl + "/vn/sales/" + sales[0].slug)
+            .click(".product-card");
+
+        const location = await t.eval(() => document.location.href);
+        await t.expect(location).notEql(config.baseUrl + "/vn");
+    }
+);
